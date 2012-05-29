1463ac239a443c59fc0627f7a9b8ba941d7b7150
Oliver Nassar <onassar@gmail.com>
2012-05-29 16:50:36 -0400

    Examples

--- example/schema.json.js
+++ example/schema.json.js
@@ -1,62 +1,30 @@
+
+// email required
 var sample = [
     {
-        "validator": ["StringValidator", "notEmpty"],
-        "failsafe": false,
-        "params": ["{name}"],
-        "error": {
-            "input": "name",
-            "message": "Please enter your name."
-        }
-    },
-    {
-        "validator": ["StringValidator", "emptyOrEmail"],
+        "validator": ["StringValidator", "email"],
         "params": ["{email}"],
         "error": {
             "input": "email",
-            "message": "Please enter a valid email address."
+            "message": "Please enter your email."
         }
-    },
-    {
-        "validator": ["StringValidator", "emptyOrUrl"],
-        "params": ["{website}"],
-        "error": {
-            "input": "website",
-            "message": "Please enter your website's url."
-        }
-    },
-    {
-        "validator": ["StringValidator", "notEmpty"],
-        "params": ["{comment}"],
-        "rules": [
-            {
-                "validator": ["StringValidator", "maxLength"],
-                "params": ["{comment}", 250],
-                "error": {
-                    "input": "comment",
-                    "message": "Comments need to be limited to 250 characters."
-                }
-            }
-
-        ],
-        "error": {
-            "input": "comment",
-            "message": "Please enter a comment."
-        }
-    },
-    {
-        "validator": ["StringValidator", "equals"],
-        "funnel": true,
-        "params": ["{updates}", "true"],
-        "rules": [
-            {
-                "validator": ["StringValidator", "email"],
-                "params": ["{email}"],
-                "error": {
-                    "input": "comment",
-                    "message": "Email must be provided if you wish to receive email updates."
-                }
-            }
-
-        ]
     }
 ];
+
+// no string required, but if provided, must be an email (funnel example)
+var sample = [
+    {
+        "validator": ["StringValidator", "notEmpty"],
+        "params": ["{email}"],
+        "funnel": true,
+        "rules": [{
+            "validator": ["StringValidator", "email"],
+            "params": ["{email}"],
+            "error": {
+                "input": "email",
+                "message": "Please enter your email."
+            }
+        }]
+    }
+];
+
